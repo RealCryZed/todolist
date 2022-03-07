d0ea4b17699cbe45397f5fc8f7691af4e186f8d3
RealCryZed <bujhmgtnhjd@bk.ru>
2022-03-07 14:19:36 +0600

    Added clock that updates time every 1 second

--- src/ru.petrov/java/MainPageController.java
+++ src/ru.petrov/java/MainPageController.java
@@ -1,22 +1,28 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainPageController extends MovableApplication {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
 
     @FXML
     private Button closeButton;
@@ -31,11 +37,15 @@ public class MainPageController extends MovableApplication {
     private TextArea taskText;
 
     @FXML
-    protected DatePicker calendar;
+    private Label clock;
+
+    @FXML
+    private DatePicker calendar;
 
     @FXML
     public void initialize() {
         calendar.setValue(LocalDate.now());
+        startClock();
     }
 
     @FXML
@@ -47,5 +57,18 @@ public class MainPageController extends MovableApplication {
     @FXML
     public void calendarAction(ActionEvent event) {
 
+    }
+
+    private void startClock() {
+        final DateFormat format = new SimpleDateFormat( "HH:mm:ss" );
+        final Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                final Calendar cal = Calendar.getInstance();
+                clock.setText(format.format(cal.getTime()));
+            }
+        }));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
     }
 }
